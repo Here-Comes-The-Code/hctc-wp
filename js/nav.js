a133ced3c61e94c4607f525f9f6aea88334e33f0
Polak, Daniel <daniel.polak@sap.com>
2022-04-07 23:57:07 +0200

    [js] reorganize js structure, remove some files, add unused folder

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+function handleNav() {
+  const mobileNavTrigger = document.querySelector('.js-mobile-nav-trigger');
+  if (mobileNavTrigger) {
+    const mobileNav = document.querySelector('.js-nav');
+    mobileNavTrigger.addEventListener('click', () => {
+      mobileNav.classList.toggle('is-active');
+    });
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line no-undef
-require('babel-register')({
-  presets: ['env'],
-});
+// require('babel-register')({
+//   presets: ['env'],
+// });
 
-// eslint-disable-next-line no-undef
-export * from './app.js';
+const init = () => {
+  handleNav();
+};
+
+init();
