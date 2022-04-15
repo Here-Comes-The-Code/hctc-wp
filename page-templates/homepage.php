35a0ee86d1111334825fa687d00b0113a99ebc60
Polak, Daniel <daniel.polak@sap.com>
2022-04-16 00:58:58 +0200

    [php] add homepage page template along with folder

---
+++ page-templates/homepage.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: HCTC - Strona główna */
+
+get_header();
+?>
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+
+  </main>
+</div>
+<?php
+// get_sidebar();
+get_footer();
