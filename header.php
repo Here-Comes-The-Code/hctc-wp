45d7f3c02d2c3bded6532a80a34fca99e63496cf
Polak, Daniel <daniel.polak@sap.com>
2022-04-20 01:51:39 +0200

    [css][php] add header top bar content info

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
  *
  * @package hctc_theme
  */
+$acf = get_field('contact-info', 'options');
 
 ?>
 <!doctype html>
@@ -25,11 +26,16 @@
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
-		<header id="masthead" class="l_site__header">
-			<div class='l_site__top-bar c-top-bar'>
+		<header id="masthead" class="l-site__header">
+			<div class='l-site__top-bar c-top-bar'>
 				<!-- top-bar-content -->
+				<a class=' c-top-bar__data c-header--icon-map-marker' target="_blank" href="https://www.google.com/maps/search/?api=1&zoom=5&query=<?php echo urlencode(strip_tags($acf['address'])) ?> ">
+					<?php echo $acf['address'] ?>
+				</a>
+				<a class='c-top-bar__data  c-header--icon-phone ' href="tel:<?php echo $acf['phone'] ?>"><?php echo $acf['phone'] ?></a>
+				<a class='c-top-bar__data  c-header--icon-envelope' href="mailto:<?php echo $acf['e-mail'] ?>"><?php echo $acf['e-mail'] ?></a>
 			</div>
-			<div class='l_site__header-main'>
+			<div class='l-site__header-main'>
 				<div class='c-logo'>
 					<?php the_custom_logo(); ?>
 				</div>
