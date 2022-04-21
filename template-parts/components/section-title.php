bcce993eeb6bbf0fc0c30897eda53539c0e91302
Polak, Daniel <daniel.polak@sap.com>
2022-04-22 01:09:30 +0200

    [css][php] add section title component

---
+++ template-parts/components/section-title.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying hero banner
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package hctc
+ */
+?>
+
+<div class='c-section-title'>
+  <a class="c-section-title__sub" href='<?php echo $acf['url']['url'] ?>' target='<?php echo $acf['url']['target'] ?>'>
+    <?php echo $args['url']['title'] ?>
+  </a>
+  <h2 class="c-section-title__main">
+    <?php echo $args['title'] ?>
+  </h2>
+</div>
