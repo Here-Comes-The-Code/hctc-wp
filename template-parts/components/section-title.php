35402ba4f35ba09e8dac35da30885324ad80136d
Polak, Daniel <daniel.polak@sap.com>
2022-04-26 02:29:36 +0200

    [php] add changes to section title class

--- template-parts/components/section-title.php
+++ template-parts/components/section-title.php
@@ -9,7 +9,7 @@
 ?>
 
 <div class='c-section-title'>
-  <a class="c-section-title__sub" href='<?php echo $acf['url']['url'] ?>' target='<?php echo $acf['url']['target'] ?>'>
+  <a class="c-section-title__sub" href='<?php echo $args['url']['url'] ?>' target='<?php echo $args['url']['target'] ?>'>
     <?php echo $args['url']['title'] ?>
   </a>
   <h2 class="c-section-title__main">
