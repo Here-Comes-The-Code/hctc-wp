c18d7cc028eb7ba365da2f951e70a4477b1414b9
Polak, Daniel <daniel.polak@sap.com>
2022-05-14 20:40:47 +0200

    [php] modify cta bar component to include variants

--- template-parts/components/cta-bar.php
+++ template-parts/components/cta-bar.php
@@ -10,28 +10,37 @@
 
 $acf_options = get_field($args['page'], 'options');
 $acf = $acf_options[$args['id']];
+$variant = $args['variant'];
 ?>
 
-<div class='c-cta-bar'>
-  <div class="c-cta-bar__content">
-    <?php echo $acf['msg'] ?>
-  </div>
-  <div class="c-cta-bar__btns">
-    <?php
-    $counter = 0;
-    foreach ($acf['btns'] as $key => $btn) : ?>
-      <a class='c-cta-bar__btn c-btn <?php
-                                      if ($counter == 0) {
-                                        echo 'c-btn--primary';
-                                      } else {
-                                        echo 'c-btn--secondary';
-                                      }
-                                      $counter++;
-                                      ?> ' href='<?php echo $btn['btn']['url']; ?>' target='<?php echo $btn['btn']['target']; ?>'>
-        <?php echo $btn['btn']['title']; ?>
-        <i class='<?php echo $btn['icon-class'] ?>'></i>
-      </a>
-    <?php endforeach; ?>
+<div class='c-cta-bar <?php if ($variant) {
+                        echo 'c-cta-bar--' . $variant;
+                      } ?>'>
+  <?php if ($variant === 'secondary') : ?>
+    <div class='c-cta-bar__wrapper'>
+    <?php endif; ?>
+    <div class="c-cta-bar__content">
+      <?php echo $acf['msg'] ?>
+    </div>
+    <div class="c-cta-bar__btns">
+      <?php
+      $counter = 0;
+      foreach ($acf['btns'] as $key => $btn) : ?>
+        <a class='c-cta-bar__btn c-btn <?php
+                                        if ($counter == 0) {
+                                          echo 'c-btn--primary';
+                                        } else {
+                                          echo 'c-btn--secondary';
+                                        }
+                                        $counter++;
+                                        ?> ' href='<?php echo $btn['btn']['url']; ?>' target='<?php echo $btn['btn']['target']; ?>'>
+          <?php echo $btn['btn']['title']; ?>
+          <i class='<?php echo $btn['icon-class'] ?>'></i>
+        </a>
+      <?php endforeach; ?>
 
-  </div>
+    </div>
+    <?php if ($variant === 'secondary') : ?>
+    </div>
+  <?php endif; ?>
 </div>
