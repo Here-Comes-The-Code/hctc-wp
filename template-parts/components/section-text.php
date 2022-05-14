65f1c89728136173e9de15cee5b4c593acf78b4a
Polak, Daniel <daniel.polak@sap.com>
2022-05-14 20:41:00 +0200

    [php] add conditional rendering to section text component

--- template-parts/components/section-text.php
+++ template-parts/components/section-text.php
@@ -9,12 +9,18 @@
  */
 ?>
 <!-- wysiwyg -->
-<div class='c-text-with-media__txt-paragraph c-text-with-media__txt-paragraph--accent'>
-  <?php echo $args['text-accent'] ?>
-</div>
-<div class='c-text-with-media__txt-paragraph '>
-  <?php echo $args['text-normal'] ?>
-</div>
-<a class="c-text-with-media__txt-link" href='<?php echo $args['read-more']['url'] ?>' target='<?php echo $args['read-more']['target'] ?>'>
-  <?php echo $args['read-more']['title'] ?>
-</a>
+<?php if ($args['text-accent']) : ?>
+  <div class='c-text-with-media__txt-paragraph c-text-with-media__txt-paragraph--accent'>
+    <?php echo $args['text-accent'] ?>
+  </div>
+<?php endif; ?>
+<?php if ($args['text-normal']) : ?>
+  <div class='c-text-with-media__txt-paragraph '>
+    <?php echo $args['text-normal'] ?>
+  </div>
+<?php endif; ?>
+<?php if ($args['read-more']) : ?>
+  <a class="c-text-with-media__txt-link" href='<?php echo $args['read-more']['url'] ?>' target='<?php echo $args['read-more']['target'] ?>'>
+    <?php echo $args['read-more']['title'] ?>
+  </a>
+<?php endif; ?>
