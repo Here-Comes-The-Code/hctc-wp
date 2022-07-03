3c1eb518078371c18448a8813eaf3693eb093c8c
Polak, Daniel <daniel.polak@sap.com>
2022-07-03 16:00:36 +0200

    [php] extend separator component

--- template-parts/home/separator.php
+++ template-parts/home/separator.php
@@ -1,8 +1,11 @@
 <?php
+$img = $args['img'];
 $title = $args['title'];
 $title_url = $args['url'];
 $read_more = $args['readmore'];
 $variant = $args['variant'];
+$class = $args['class'];
+$color = $args['color'];
 $class = $args['class'];
 if ($title_url) {
     $title_url = $args['url']['url'];
@@ -10,14 +13,32 @@ if ($title_url) {
 
 ?>
 
-<div class='c-separator <?php if ($variant) {
-                            echo "c-separator--" . $variant;
-                        }; ?> <?php if ($class) {
-                                    echo $class;
-                                }; ?>'>
+<?php if ($color) : ?>
+    <style>
+        .c-separator.color:after {
+            background-color: <?php echo $color; ?>
+        }
+
+        .c-separator.color .c-separator__button:not(.c-separator__button--alt) {
+            background-color: <?php echo $color; ?>
+        }
+
+        .c-separator.color .c-separator__button--alt {
+            color: <?php echo $color; ?>
+        }
+    </style>
+<?php endif; ?>
+<div class='c-separator  <?php if ($variant) {
+                                echo "c-separator--" . $variant;
+                            }; ?> <?php if ($class) {
+                                        echo $class;
+                                    }; ?>'>
     <a class='c-separator__button' <?php if ($title_url) {
                                         echo 'href="' . $title_url . '"';
                                     } ?>>
+        <?php if ($img) : ?>
+            <img src="<?php echo $img ?>" alt="">
+        <?php endif; ?>
         <?php echo $title ?>
     </a>
     <?php if ($read_more) : ?>
